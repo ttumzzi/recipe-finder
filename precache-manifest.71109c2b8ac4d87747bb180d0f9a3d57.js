ca9f8cb69511e863570b7aa15e0e90ffa731a1ec
ttumzzi <ssumini0829@gmail.com>
2020-04-24 00:09:52 +0900

    Updates

---
+++ precache-manifest.71109c2b8ac4d87747bb180d0f9a3d57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b5bb260317f08cf65033065f1e5692",
+    "url": "/recipe-finder/index.html"
+  },
+  {
+    "revision": "51976018e6d0a82f101e",
+    "url": "/recipe-finder/static/css/2.2dc38268.chunk.css"
+  },
+  {
+    "revision": "95c77cc9e2b5847bd522",
+    "url": "/recipe-finder/static/css/main.ebc3d84a.chunk.css"
+  },
+  {
+    "revision": "51976018e6d0a82f101e",
+    "url": "/recipe-finder/static/js/2.849489ff.chunk.js"
+  },
+  {
+    "revision": "e23d72e353070f497294c1592aa53554",
+    "url": "/recipe-finder/static/js/2.849489ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c77cc9e2b5847bd522",
+    "url": "/recipe-finder/static/js/main.6fb89aad.chunk.js"
+  },
+  {
+    "revision": "25fa34a5b61c54fe0fb3",
+    "url": "/recipe-finder/static/js/runtime-main.266945a1.js"
+  }
+]);
